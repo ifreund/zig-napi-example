2504dcf69c2e21a2d84556f99ba19d90f5ff7885
Isaac Freund <mail@isaacfreund.com>
2022-05-11 22:26:36 +0200

    Update for zig 0.9

--- example.zig
+++ example.zig
@@ -4,12 +4,12 @@ const c = @cImport({
 
 export fn napi_register_module_v1(env: c.napi_env, exports: c.napi_value) c.napi_value {
     var function: c.napi_value = undefined;
-    if (c.napi_create_function(env, null, 0, foo, null, &function) != .napi_ok) {
+    if (c.napi_create_function(env, null, 0, foo, null, &function) != c.napi_ok) {
         _ = c.napi_throw_error(env, null, "Failed to create function");
         return null;
     }
 
-    if (c.napi_set_named_property(env, exports, "foo", function) != .napi_ok) {
+    if (c.napi_set_named_property(env, exports, "foo", function) != c.napi_ok) {
         _ = c.napi_throw_error(env, null, "Failed to add function to exports");
         return null;
     }
@@ -18,8 +18,10 @@ export fn napi_register_module_v1(env: c.napi_env, exports: c.napi_value) c.napi
 }
 
 fn foo(env: c.napi_env, info: c.napi_callback_info) callconv(.C) c.napi_value {
+    _ = info;
+
     var result: c.napi_value = undefined;
-    if (c.napi_create_int32(env, 42, &result) != .napi_ok) {
+    if (c.napi_create_int32(env, 42, &result) != c.napi_ok) {
         _ = c.napi_throw_error(env, null, "Failed to create return value");
         return null;
     }
